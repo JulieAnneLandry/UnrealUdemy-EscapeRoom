38217924ffeac5ab83e62e7d90188dcf2ba08a23
Julie-Anne Landry <julieannelandry94@gmail.com>
2018-11-23 11:18:17 -0500

    V71. Laying Out Geometry

--- EscapeRoom/Source/EscapeRoom/OpenDoor.cpp
+++ EscapeRoom/Source/EscapeRoom/OpenDoor.cpp
@@ -19,9 +19,9 @@ void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
 
-	AActor* Owner = GetOwner();
+	/*AActor* Owner = GetOwner();
 	FRotator NewRotation = FRotator(0.0f, 60.0f, 0.0f);
-	Owner->SetActorRotation(NewRotation);
+	Owner->SetActorRotation(NewRotation);*/
 
 }
 
