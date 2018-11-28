b671717d751369321aa8e64cdb100ac2a53c710e
Julie-Anne Landry <julieannelandry94@gmail.com>
2018-11-28 12:10:47 -0500

    V.97 Pointer Protection Process

--- EscapeRoom/Source/EscapeRoom/OpenDoor.cpp
+++ EscapeRoom/Source/EscapeRoom/OpenDoor.cpp
@@ -23,8 +23,11 @@ UOpenDoor::UOpenDoor()
 void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
-
 	Owner = GetOwner();
+	if (!PressurePlate)
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s missing pressure plate"), *GetOwner()->GetName());
+	}
 }
 
 void UOpenDoor::OpenDoor()
@@ -65,6 +68,7 @@ float UOpenDoor::GetTotalMassOfActorsOnPlate()
 
 	// Find all the overlapping actors
 	TArray<AActor*> OverlappingActors;
+	if (!PressurePlate) { return TotalMass; }
 	PressurePlate->GetOverlappingActors(OUT OverlappingActors);
 
 	// Iterate through them adding their masses
