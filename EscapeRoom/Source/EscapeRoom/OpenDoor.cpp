c0ea3dada492e24e4b9ad9524f261dc352e134fc
Julie-Anne Landry <julieannelandry94@gmail.com>
2018-11-28 13:16:29 -0500

    V98. Exposing Events to Blueprint

--- EscapeRoom/Source/EscapeRoom/OpenDoor.cpp
+++ EscapeRoom/Source/EscapeRoom/OpenDoor.cpp
@@ -32,12 +32,13 @@ void UOpenDoor::BeginPlay()
 
 void UOpenDoor::OpenDoor()
 {
-	Owner->SetActorRotation(FRotator(0.0f, OpenAngle, 0.0f));
+	// Owner->SetActorRotation(FRotator(0.0f, OpenAngle, 0.0f));
+	OnOpenRequest.Broadcast();
 }
 
 void UOpenDoor::CloseDoor()
 {
-	Owner->SetActorRotation(FRotator(0.0f, 0.0f, 0.0f));
+	Owner->SetActorRotation(FRotator(0.0f, -90.0f, 0.0f));
 }
 
 
